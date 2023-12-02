3b621cdfb63ae8153db2621267c8501490ca3201
padmarahul <64130719+padmarahul@users.noreply.github.com>
2023-12-01 22:52:28 -0600

    Update OTPVerification.js

--- src/pages/OTPVerification.js
+++ src/pages/OTPVerification.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Link, useNavigate } from "react-router-dom";
-import "../pages/OTPVerification.js";
+import "../pages/OTPVerification.css";
 
 const LOCKOUT_DURATION = 30000; // Lockout duration in milliseconds (30 seconds)
 
