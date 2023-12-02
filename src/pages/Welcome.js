e2527abd8450101b4f6de25dee2ad585023054d6
padmarahul <64130719+padmarahul@users.noreply.github.com>
2023-12-01 23:49:30 -0600

    Update Welcome.js

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-//import Slider from 'react-slick';
-//import 'slick-carousel/slick/slick.css';
-//import 'slick-carousel/slick/slick-theme.css';
 import './Welcome.css';
-import caveImage from '../images/cave.jpeg';
-import geminiArtImage from '../images/geminiart.jpeg';
-import monaLisaImage from '../images/astronaut...jpeg';
-import imh1 from '../images/vintagecar.jpeg';
 
 
 function Welcome() {
